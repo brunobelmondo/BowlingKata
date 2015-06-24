98feb63476d246d44b632458cfc5c4ce5a8424a4
brunobelmondo <bruno.belmondo@gmail.com>
2015-06-24 15:27:51 +0200

    remove duplicated code

--- F#/BowlingKata/Bowling.fs
+++ F#/BowlingKata/Bowling.fs
@@ -2,19 +2,24 @@
 
 module Bowling = 
 
-    type Game = {CurrentFrame:int; Rolls:int List; Score: int}
+    type Game = {CurrentFrame:int; Rolls:int List; Score: int} 
+
+    let CountFrame game remainingRolls frameScore = 
+        {CurrentFrame=game.CurrentFrame+1; Rolls=remainingRolls; Score=game.Score + frameScore}
 
     let ScoreLastFrame game = 
+        let countFrameOnGame = CountFrame game []
         match game.Rolls with
-        | 10::bonus1::bonus2::[] -> {CurrentFrame=game.CurrentFrame; Rolls=[]; Score=game.Score + 10 + bonus1 + bonus2}
-        | roll1::roll2::bonus::[] -> {CurrentFrame=game.CurrentFrame; Rolls=[]; Score=game.Score + 10 + bonus}
-        | roll1::roll2::[] -> {CurrentFrame=game.CurrentFrame; Rolls=[]; Score=game.Score + roll1 + roll2}
+        | 10::bonus1::bonus2::[] -> countFrameOnGame (10 + bonus1 + bonus2)
+        | roll1::roll2::bonus::[] -> countFrameOnGame (10 + bonus)
+        | roll1::roll2::[] -> countFrameOnGame (roll1 + roll2)
 
     let ScoreFrame game = 
+        let countFrameOnGame = CountFrame game
         match game.Rolls with
-        | 10::bonus1::bonus2::rest -> {CurrentFrame=game.CurrentFrame + 1; Rolls=bonus1::bonus2::rest; Score=game.Score + 10 + bonus1 + bonus2}
-        | roll1::roll2::bonus::rest when roll1 + roll2 = 10-> {CurrentFrame=game.CurrentFrame + 1; Rolls=bonus::rest; Score=game.Score + 10 + bonus}
-        | roll1::roll2::rest -> {CurrentFrame=game.CurrentFrame + 1; Rolls=rest; Score=game.Score + roll1 + roll2}
+        | 10::bonus1::bonus2::rest -> countFrameOnGame (bonus1::bonus2::rest) (10 + bonus1 + bonus2)
+        | roll1::roll2::bonus::rest when roll1 + roll2 = 10-> countFrameOnGame (bonus::rest) (10 + bonus)
+        | roll1::roll2::rest -> countFrameOnGame rest (roll1 + roll2)
 
     let rec ScoreGame game = 
         match game.CurrentFrame with
