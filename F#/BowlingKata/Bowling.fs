a9166417dd4b69c9728f10af3dda2f4350f68c43
brunobelmondo <bruno.belmondo@gmail.com>
2015-06-24 15:39:34 +0200

    simplification

--- F#/BowlingKata/Bowling.fs
+++ F#/BowlingKata/Bowling.fs
@@ -4,18 +4,13 @@ module Bowling =
 
     type Game = {CurrentFrame:int; Rolls:int List; Score: int} 
 
-    let CountFrame game remainingRolls frameScore = 
-        {CurrentFrame=game.CurrentFrame+1; Rolls=remainingRolls; Score=game.Score + frameScore}
-
     let ScoreLastFrame game = 
-        let countFrameOnGame = CountFrame game []
-        match game.Rolls with
-        | 10::bonus1::bonus2::[] -> countFrameOnGame (10 + bonus1 + bonus2)
-        | roll1::roll2::bonus::[] -> countFrameOnGame (10 + bonus)
-        | roll1::roll2::[] -> countFrameOnGame (roll1 + roll2)
+        let frameScore = List.sum game.Rolls
+        {CurrentFrame=game.CurrentFrame; Rolls=[]; Score=game.Score + frameScore}
 
     let ScoreFrame game = 
-        let countFrameOnGame = CountFrame game
+        let countFrameOnGame remainingRolls frameScore= 
+            {CurrentFrame=game.CurrentFrame+1; Rolls=remainingRolls; Score=game.Score + frameScore}
         match game.Rolls with
         | 10::bonus1::bonus2::rest -> countFrameOnGame (bonus1::bonus2::rest) (10 + bonus1 + bonus2)
         | roll1::roll2::bonus::rest when roll1 + roll2 = 10-> countFrameOnGame (bonus::rest) (10 + bonus)
