d490aefc28c9eab271a55c02699b4a1f8303dad0
brunobelmondo <bruno.belmondo@gmail.com>
2015-06-24 15:16:34 +0200

    initial commit

---
+++ F#/BowlingKata/Bowling.fs
@@ -0,0 +1,26 @@
+﻿namespace BowlingKata
+
+module Bowling = 
+
+    type Game = {CurrentFrame:int; Rolls:int List; Score: int}
+
+    let ScoreLastFrame game = 
+        match game.Rolls with
+        | 10::bonus1::bonus2::[] -> {CurrentFrame=game.CurrentFrame; Rolls=[]; Score=game.Score + 10 + bonus1 + bonus2}
+        | roll1::roll2::bonus::[] -> {CurrentFrame=game.CurrentFrame; Rolls=[]; Score=game.Score + 10 + bonus}
+        | roll1::roll2::[] -> {CurrentFrame=game.CurrentFrame; Rolls=[]; Score=game.Score + roll1 + roll2}
+
+    let ScoreFrame game = 
+        match game.Rolls with
+        | 10::bonus1::bonus2::rest -> {CurrentFrame=game.CurrentFrame + 1; Rolls=bonus1::bonus2::rest; Score=game.Score + 10 + bonus1 + bonus2}
+        | roll1::roll2::bonus::rest when roll1 + roll2 = 10-> {CurrentFrame=game.CurrentFrame + 1; Rolls=bonus::rest; Score=game.Score + 10 + bonus}
+        | roll1::roll2::rest -> {CurrentFrame=game.CurrentFrame + 1; Rolls=rest; Score=game.Score + roll1 + roll2}
+
+    let rec ScoreGame game = 
+        match game.CurrentFrame with
+        |10 -> ScoreLastFrame game
+        |_ ->  ScoreGame (ScoreFrame game) 
+
+    let Score (rolls:int List) = 
+        let scoredGame = ScoreGame {CurrentFrame = 1; Rolls = rolls; Score = 0}
+        scoredGame.Score
---
+++ F#/BowlingKata/BowlingTest.fs
@@ -0,0 +1,39 @@
+﻿namespace BowlingKata
+
+open NUnit.Framework
+open Bowling
+
+[<TestFixture>]
+type BowlingTests() = 
+    let EmptyFrame = [0;0]
+    let EmptyFrames number = List.replicate number EmptyFrame 
+    let EmptyGame number = List.concat (EmptyFrames number)
+
+    [<Test>]
+    member x.Should_score_be_0_when_empty_game() =
+        Assert.AreEqual(0, EmptyGame 10 |> Score)
+
+    [<Test>]
+    member x.Should_score_be_sum_of_pins_when_last_frame() =
+        let game = {CurrentFrame = 10; Rolls=[4;5]; Score=0}
+        let scoredGame = ScoreGame game
+        Assert.AreEqual(9, scoredGame.Score)
+
+    [<Test>]
+    member x.Should_score_be_sum_of_pins_and_bonus_when_spare_on_last_frame() =
+        let game = {CurrentFrame = 10; Rolls=[5;5;4]; Score=0}
+        let scoredGame = ScoreGame game
+        Assert.AreEqual(14, scoredGame.Score)
+
+    [<Test>]
+    member x.Should_score_be_sum_of_pins_and_bonus_when_strike_on_last_frame() =
+        let game = {CurrentFrame = 10; Rolls=[10;5;4]; Score=0}
+        let scoredGame = ScoreGame game
+        Assert.AreEqual(19, scoredGame.Score)
+
+    [<Test>]
+    member x.Should_score_be_sum_of_pins_when_normal_frame() =
+        let game = {CurrentFrame = 9; Rolls=[5;4;0;0]; Score=0}
+        let scoredGame = ScoreGame game
+        Assert.AreEqual(9, scoredGame.Score)
+
